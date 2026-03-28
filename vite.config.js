4c5801f70ccd9d04b7b0b3ba97696518868ad367
User1 <user1@local>
2026-03-28 22:11:24 +0500

    fix: update base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/react-sheet-red/cyberpunk-red/',
+  base: '/react-sheet-red/',
   server: {
     port: 3000,
     open: true

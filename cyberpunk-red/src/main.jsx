4d4a91b4b253ac978547b7359299302c68f02181
User1 <user1@local>
2026-03-28 20:44:50 +0500

    Update config for Github Pages

--- cyberpunk-red/src/main.jsx
+++ cyberpunk-red/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
 import { CharacterProvider } from './context/CharacterContext.jsx'
 import { LanguageProvider } from './context/LanguageContext.jsx'
@@ -8,12 +8,12 @@ import './css/common.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <LanguageProvider>
         <CharacterProvider>
           <App />
         </CharacterProvider>
       </LanguageProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 )
